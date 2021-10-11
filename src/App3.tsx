9f9409b46c01bf972777dfffb9ab7fa741b8de40
Necip <ncptzgn@gmail.com>
2021-10-11 12:25:41 +0300

    TypeScript Tutorial with react-redux

---
+++ src/App3.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App3 = () => {
+    return (
+        <div>
+            <h1>0</h1>
+        </div>
+    )
+}
+
+export default App3
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import {render} from "react-dom"
 import App from "./App"
 import App2 from "./App2"
+import App3 from "./App3"
 import "./index.css"
+import { Provider} from "react-redux"
+import { store } from "./state"
 
 const root = document.querySelector("#root")
 
-render(<App2/>,root)
+render(
+    <Provider store={store}>
+       <App3/> 
+    </Provider>
+
+,root)
