b324c6a73d5db8454dd6b29c8268b104c8ab5c3f
Necip <ncptzgn@gmail.com>
2021-10-11 12:35:10 +0300

    TypeScript Tutorial with react-redux

--- src/App3.tsx
+++ src/App3.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useDispatch , useSelector} from "react-redux"
+import { bindActionCreators } from 'redux';
+import { actionCreators,State } from './state';
 
 const App3 = () => {
+
+    const dispatch = useDispatch();
+
+    const {depositMoney,withdrawMoney,bankrupt} = bindActionCreators(actionCreators,dispatch)
+
+    const amount = useSelector((state:State)=>state.bank)
     return (
         <div>
-            <h1>0</h1>
+            <h1>{amount}</h1>
+            <button onClick={()=>{depositMoney(1000)}}>deposit</button>
+            <button onClick={()=>{withdrawMoney(500)}}>withdraw</button>
+            <button onClick={()=>{bankrupt()}}>bankrupt</button>
         </div>
     )
 }
